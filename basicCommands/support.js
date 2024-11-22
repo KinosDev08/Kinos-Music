3b02dae4ad66e57000b8b82b2de22d979f00e582
KinosDev08 <anhkhoikieu8@gmail.com>
2024-11-23 02:49:35 +0700

    Update support.js

--- basicCommands/support.js
+++ basicCommands/support.js
@@ -11,9 +11,9 @@ module.exports = {
         const embed = new EmbedBuilder()
             .setColor('#b300ff')
             .setAuthor({
-              name: 'Support Server',
+              name: 'Contact For Support',
               iconURL: 'https://cdn.discordapp.com/attachments/1230824451990622299/1230824519220985896/6280-2.gif?ex=6638ae28&is=66375ca8&hm=13e4a1b91a95b2934a39de1876e66c11711c7b30ac1a91c2a158f2f2ed1c2fc6&', 
-              url: 'https://discord.gg/xQF9f9yUEM'
+              url: 'https://www.facebook.com/KinosMMO'
           })
             .setDescription(`➡️ **Join our Discord server for support and updates:**\n- Discord - ${supportServerLink}\n\n➡️ **Follow us on:**\n- GitHub - ${githubLink}\n- Instagram - ${instagramLink}\n- Facebook - ${facebookLink}`)
             .setImage('https://cdn.discordapp.com/attachments/1113800537402527903/1236803979996958740/11.png?ex=663956f7&is=66380577&hm=3b3c19a11adcb979517a133f2907f671305d23f1f5092cf7df043e6d5cab07bc&')
@@ -26,20 +26,12 @@ module.exports = {
 
 /*
 
-  ________.__                        _____.___.___________
- /  _____/|  | _____    ____  ____   \__  |   |\__    ___/
-/   \  ___|  | \__  \ _/ ___\/ __ \   /   |   |  |    |   
-\    \_\  \  |__/ __ \\  \__\  ___/   \____   |  |    |   
- \______  /____(____  /\___  >___  >  / ______|  |____|   
-        \/          \/     \/    \/   \/                  
-
 ╔════════════════════════════════════════════════════════════════════════╗
-║                                                                        ║
-║  ## Created by GlaceYT!                                                ║
-║  ## Feel free to utilize any portion of the code                       ║
-║  ## DISCORD :  https://discord.com/invite/xQF9f9yUEM                   ║
-║  ## YouTube : https://www.youtube.com/@GlaceYt                         ║
-║                                                                        ║
+║                                                                                     ║
+║  ## Created by KinosDev!                                                            ║
+║  ## Facebook :  https://www.facebook.com/KinosMMO                                   ║
+║  © Copyright 2024 · Coder by Kieu Anh Khoi                                          ║
+║                                                                                     ║
 ╚════════════════════════════════════════════════════════════════════════╝
 
 
