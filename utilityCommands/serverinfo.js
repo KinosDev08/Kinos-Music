93f4ea25ace54abaa82caaea508437f44303a89a
KinosDev08 <anhkhoikieu8@gmail.com>
2024-11-23 02:54:50 +0700

    Update serverinfo.js

--- utilityCommands/serverinfo.js
+++ utilityCommands/serverinfo.js
@@ -39,21 +39,11 @@ module.exports = {
 
 /*
 
-   MADE BY RTX!! FEEL FREE TO USE ANY PART OF CODE
-
-  ██████╗░████████╗██╗░░██╗           
-  ██╔══██╗╚══██╔══╝╚██╗██╔╝          
-  ██████╔╝░░░██║░░░░╚███╔╝░          
-  ██╔══██╗░░░██║░░░░██╔██╗░          
-  ██║░░██║░░░██║░░░██╔╝╚██╗          
-  ╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝          
-
-   FOR EMOJIS EITHER YOU CAN EDIT OR JOIN OUR DISCORD SERVER 
-   SO WE ADD BOT TO OUR SERVER SO YOU GET ANIMATED EMOJIS.
-
-   DISCORD SERVER : https://discord.gg/FUEHs7RCqz
-   YOUTUBE : https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A
-
-   FOR HELP CONTACT ME ON DISCORD
-   ## Contact    [ DISCORD SERVER :  https://discord.gg/c4kaW2sSbm ]
+╔════════════════════════════════════════════════════════════════════════╗
+║                                                                                     ║
+║  ## Created by KinosDev!                                                            ║
+║  ## Facebook :  https://www.facebook.com/KinosMMO                                   ║
+║  © Copyright 2024 · Coder by Kieu Anh Khoi                                          ║
+║                                                                                     ║
+╚════════════════════════════════════════════════════════════════════════╝
 */
